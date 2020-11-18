e24aa60b027ad6f21f9cf7dff960c784cd3856b3
Edmar159 <dema159@msn.com>
2020-11-18 06:53:51 -0300

    Commit inicial.
    Por se tratar de projeto simples, possui todas as alterações necessárias para atender requisitos.
    https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from './services/api';
 
 function App() {
   async function handleAddRepository() {
-    // TODO
+    // Adicionar um repositório a sua API: Deve ser capaz de adicionar um novo item na sua API através de um botão com o texto Adicionar
+    // e, após a criação, deve ser capaz de exibir o nome dele após o cadastro.
+    api.post('repositories', {
+      title: `novo projeto ${Date.now()}`,
+      url: 'http://github.com/',
+      techs: ''
+  })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // Remover um repositório da sua API: Para cada item da sua lista, deve possuir um botão com o texto Remover que, 
+    // ao clicar, irá chamar uma função para remover esse item da lista do seu frontend e da sua API.
+    api.delete(`/repositories/`+id);
   }
 
+  const [repositories, setRepositories] = useState ([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+},[handleAddRepository,handleRemoveRepository]) 
+  
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+       {repositories.map(repository => <li key = {repository.id}> {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
