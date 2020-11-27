2e4f3fde7298f826399117f05d6adcf5ee273006
Edmar159 <dema159@msn.com>
2020-11-27 06:38:46 -0300

    Update App.js
    Adicionado variável de retorno da API

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   async function handleAddRepository() {
     // Adicionar um repositório a sua API: Deve ser capaz de adicionar um novo item na sua API através de um botão com o texto Adicionar
     // e, após a criação, deve ser capaz de exibir o nome dele após o cadastro.
-    api.post('repositories', {
+    const response = await api.post('repositories', {
       title: `novo projeto ${Date.now()}`,
       url: 'http://github.com/',
-      techs: ''
+      techs: ['Node.js','ReactJS']
   })
 
     setRepositories([...repositories,response.data]);
@@ -19,7 +19,7 @@ function App() {
   async function handleRemoveRepository(id) {
     // Remover um repositório da sua API: Para cada item da sua lista, deve possuir um botão com o texto Remover que, 
     // ao clicar, irá chamar uma função para remover esse item da lista do seu frontend e da sua API.
-    api.delete(`/repositories/`+id);
+    const response = await api.delete(`/repositories/${id}`);
     
     setRepositories(repositories.filter(repo => repo.id !== id));
   }
